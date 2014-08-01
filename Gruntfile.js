986fbb4c95a26d6027153fca47addf7c99190f7a
Sean Clayton <sean@skulbuny.com>
2014-08-01 11:42:41 -0400

    Removed changelog stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,34 +2,15 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    git_changelog: {
-      minimal: {
-        options: {
-          repo_url: 'git@skulbuny.com:sean/rabit.git',
-          appName : 'Git changelog',
-          file: 'CHANGELOG.md'
-        }
-      },
-      extended: {
-        options: {
-          repo_url: 'https://github.com/rafinskipg/git-changelog',
-          appName: 'Git changelog extended',
-          file: 'EXTENDEDCHANGELOG.md',
-          grep_commits: '^fix|^feat|^docs|^refactor|^chore|BREAKING'
-        }
-      }
-    },
     watch: {
       files: ['assets/sass/*.scss'],
       tasks: ['sass']
     }
   });
 
-  grunt.loadNpmTasks('git-changelog');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-sass');
 
   grunt.registerTask('dev', ['watch']);
-  grunt.registerTask('changelog', ['git_changelog']);
 
 };
