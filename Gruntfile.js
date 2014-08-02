2dcdff132866539ab39f3c5fad725aee75045b70
Sean Clayton <sean@skulbuny.com>
2014-08-02 10:25:58 -0400

    Added js uglification

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
-      files: ['assets/sass/*.scss'],
-      tasks: ['sass']
+      sass: {
+        files: ['assets/sass/*.scss'],
+        tasks: ['sass']
+      },
+      js: {
+        files: ['assets/js/scripts/*.js'],
+        tasks: ['uglfiy']
+      }
     },
     sass: {
       dist: {
@@ -15,12 +21,24 @@ module.exports = function(grunt) {
           outputStyle: 'compressed'
         }
       }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/rabit.min.js': ['assets/js/scripts/*.js']
+        },
+        options: {
+          compress: true,
+          mangle: false
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-sass');
 
-  grunt.registerTask('dev', ['sass', 'watch']);
+  grunt.registerTask('dev', ['uglify', 'sass', 'watch']);
 
 };
