1481106c4cd7a46a309e08d54f7a37fdacc472e6
Sean Clayton <sean@skulbuny.com>
2014-08-03 12:20:32 -0400

    Reload highlighter on new content

--- assets/js/scripts/scripts.js
+++ assets/js/scripts/scripts.js
@@ -70,7 +70,7 @@ jQuery(function($) {
 
         // Re run fitvid.js
         $newContent.fitVids();
-        hljs.highlightBlock($newContent);
+        $newContent.initHighlightingOnLoad();
 
         $ajaxContainer.html($newContent);
         $ajaxContainer.fadeIn(500);
