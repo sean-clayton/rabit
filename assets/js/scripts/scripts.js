a993d8cb9f00e64fa9c841de3067797a90594790
Sean Clayton <sean@skulbuny.com>
2014-08-07 11:33:19 -0400

    Updated gruntfile to include mangling and initialized hilightjs

--- assets/js/scripts/scripts.js
+++ assets/js/scripts/scripts.js
@@ -2,6 +2,8 @@ jQuery(function($) {
 
   InstantClick.init(50, true);
 
+  hljs.initHighlightingOnLoad();
+
   $('.js-jump-top').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({'scrollTop': 0});
