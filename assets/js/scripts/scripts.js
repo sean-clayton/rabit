efac0df0dbd5bd266583fcb78192031a203e2d47
Sean Clayton <sean@skulbuny.com>
2014-08-07 11:49:01 -0400

    Fixed syntax highlighting not working

--- assets/js/scripts/scripts.js
+++ assets/js/scripts/scripts.js
@@ -2,8 +2,6 @@ jQuery(function($) {
 
   InstantClick.init(50, true);
 
-  hljs.initHighlightingOnLoad();
-
   $('.js-jump-top').on('click', function(e) {
     e.preventDefault();
     $('html, body').animate({'scrollTop': 0});
